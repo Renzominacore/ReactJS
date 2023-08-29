6d137aa30e1ba259092a2596198bad9404cbbf8d
Renzominacore <minacorerenzo@gmail.com>
2023-08-29 14:45:01 -0300

    EntregaProyecto

--- src/componentes/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/componentes/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,6 +1,27 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { getProduct } from "../../services";
+import ItemDetail from "./ItemDetail";
 
 const ItemDetailContainer = () => {
-    return <div>ItemDetailContainer</div>
+  const [item, setItem] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const { id } = useParams();
+
+  useEffect(() => {
+    getProduct(id)
+      .then((response) => {
+        setItem(response);
+      })
+      .catch(() => {
+        setItem(null);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [id]);
+
+  return <ItemDetail item={item} isLoading={isLoading} />;
 };
+
 export default ItemDetailContainer;
